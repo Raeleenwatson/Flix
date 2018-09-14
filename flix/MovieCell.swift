49ab1486b3e5ed0a530ed32121ee3b6b0d48d320
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-09-14 09:48:10 -0700

    Saving files

---
+++ flix/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  flix
+//
+//  Created by Raeleen Watson on 9/14/18.
+//  Copyright © 2018 Raeleen Watson. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- flix/NowPlayingViewController.swift
+++ flix/NowPlayingViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
-    
+    @IBOutlet weak var tableView: UITableView!
+    var movies: [[String:Any]] = []
     
     override func viewDidLoad() {
+        
+        tableView.dataSource = self
         super.viewDidLoad()
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -25,10 +28,7 @@ class NowPlayingViewController: UIViewController {
             } else if let data = data {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 let movies = dataDictionary["results"] as! [[String: Any]]
-                for movie in movies{
-                    let title = movie["title"] as! String
-                    print(title)
-                }
+                self.movies = movies
                 
                 // TODO: Get the array of movies
                 // TODO: Store the movies in a property to use elsewhere
@@ -39,7 +39,19 @@ class NowPlayingViewController: UIViewController {
         task.resume()
 
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        
+        let movies = movie[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
