1b655aaf74d7899072c2fad30344b166e31a1f8e
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-09-17 22:11:32 -0700

    Added new files

---
+++ flix/ViewControllers/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Raeleen Watson on 9/17/18.
+//  Copyright © 2018 Raeleen Watson. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
