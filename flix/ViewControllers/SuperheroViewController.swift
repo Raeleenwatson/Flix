480556d32c2f28b579e70e8bbdaaabb295825557
Raeleen Watson <raeleenwatson@Raeleens-MacBook-Pro.local>
2018-09-18 17:32:06 -0700

    Finish changes

--- flix/ViewControllers/SuperheroViewController.swift
+++ flix/ViewControllers/SuperheroViewController.swift
@@ -18,8 +18,19 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumLineSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width /  cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        
+        
         fetchMovies()
-        // Do any additional setup after loading the view.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
